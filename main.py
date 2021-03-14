81094bd6542f993e05190181d9e02ce3ef79ae43
Rahul Dutt <rdutt@actonfiji.com>
2021-03-14 20:38:21 +1200

    Added Calculations To get Correct Weight

--- main.py
+++ main.py
@@ -2,56 +2,59 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# Read data from csv file
 def load_data():
-    _data = pd.read_csv('data.csv', header=None)
-    _data = _data[:100]
-    _data = np.asmatrix(_data, dtype='float64')
-    return _data
+    # get data as arrays
+    loaded_data = pd.read_csv('data.csv', header=None)
+    # convert arrays to matrix
+    data_matrix = np.asmatrix(loaded_data, dtype='float64')
+    return data_matrix
 
 
-def plot_data(_data):
+def plot_data(_data, _weights):
     plt.scatter(np.array(_data[:50, 0]), np.array(_data[:50, 1]), marker='o', label='setosa')
-    plt.scatter(np.array(_data[50:, 0]), np.array(_data[50:, 1]), marker='x', label='versicolor')
+    plt.scatter(np.array(_data[50:, 0]), np.array(_data[50:, 1]), marker='+', label='versicolor')
     plt.xlabel('petal length')
     plt.ylabel('sepal length')
     plt.legend()
+
+    w = _weights.tolist()[0]
+    x1 = [x1 for x1 in range(4, 8)]
+    print(x1)
+    x2 = [(w[0] + w[1] * i) / (-w[2]) for i in x1]
+    print(x2)
+    plt.plot(x1, x2)
+
     plt.show()
 
 
-def perceptron(_data, _num_iter):
-    features = _data[:, 0:1]
+def perceptron(_data):
+    features = _data[:, 0:2]
     labels = _data[:, -1]
+    learning_rate = 0.5
 
-    # set weights to zero
+    # Initialize 'w' with zeros
     w = np.zeros(shape=(1, features.shape[1] + 1))
 
-    __misclassified = []
-
-    for epoch in range(_num_iter):
+    # Iterate 10 times and Update 'w' 
+    for i in range(10):
         misclassified = 0
         for x, label in zip(features, labels):
             x = np.insert(x, 0, 1)
             y = np.dot(w, x.transpose())
-            target = 1.0 if (y > 0) else 0.0
 
-            delta = (label.item(0, 0) - target)
+            prediction = 1.0 if y > 0 else -1.0
+            delta = learning_rate * (label.item(0, 0) - prediction)
 
-            if delta:  # misclassified
+            if delta:
                 misclassified += 1
                 w += (delta * x)
-
-        __misclassified.append(misclassified)
-    return w, __misclassified
+        # Just a Cool Output
+        print(f'Iteration [{i}] - Miscalculation Count - {misclassified}')
+    return w
 
 
 data = load_data()
-plot_data(data)
-print(data)
-num_iter = 10
-w, misclassified_ = perceptron(data, num_iter)
-
-epochs = np.arange(1, num_iter+1)
-plt.plot(epochs, misclassified_)
-plt.xlabel('iterations')
-plt.ylabel('misclassified')
-plt.show()
+weights = perceptron(data)
+plot_data(data, weights)
